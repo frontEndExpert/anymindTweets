31f78d8839f591c1496d7fdbb807100355d96fd8
fronendexpert <aylon.spigel@gmail.com>
2018-09-02 22:43:05 +0700

    both working well

--- src/app/tweet-service.service.ts
+++ src/app/tweet-service.service.ts
@@ -31,16 +31,6 @@ export class TweetServiceService {
   constructor(private http:Http) { }
 
 
-  //  getAllTweets(hashtag: string): Observable<Object> {
-  //   let myUrl="https://am-twitter-scrape.herokuapp.com/hashtags/"+hashtag+"?pages_limit=10&wait=0";
-  //   console.log('myUrl',myUrl);
-  //   const myObservableTweets =  this.http.get( myUrl  );
-  //       // .subscribe((data) => this.allTweets2=data); 
-  //       //console.log('this.allTweets2',this.allTweets2);
-  //     return myObservableTweets ;
-
-  // }
-  
   getAllTweets(hashtag:string) {
     let promise = new Promise((resolve, reject) => {
       let apiURL = `https://am-twitter-scrape.herokuapp.com/hashtags/${hashtag}?pages_limit=10&wait=0`;
@@ -58,25 +48,30 @@ export class TweetServiceService {
             }
         );
     });
-    console.log('promise',promise);
+    //console.log('promise',promise);
     return promise;
   }
 
-  getAllUserTweets(): Observable<string> {
-
-    const myUserObservableTweets = Observable.create((observer: Observer<string>) => {
-     
-      for(let i=0;i<this.allUserTweets.length;i++){
-        setTimeout(() => {
-          observer.next(JSON.stringify(this.allUserTweets[i]));
-        }, 1000);
-      }
-  });
-      return myUserObservableTweets ;
-
-      //const myObservableTweets = this.http.get('https://am-twitter-scrape.herokuapp.com/hashtags/Python?pages_limit=3&wait=0'    );
-         // .subscribe((data) => this.allTweets2=data); 
-      //console.log('this.allTweets2',this.allTweets2);
+  getTweetsByUser(userName:string){
+    let promise = new Promise((resolve, reject) => {
+      let apiURL = `https://am-twitter-scrape.herokuapp.com/users/${userName}?pages_limit=10&wait=0`;
+    //  console.log('apiURL',apiURL);
+      this.http.get(apiURL)
+        .toPromise()
+        .then(
+          res => { // Success
+            //console.log('res.json()', res.json());
+            this.results = res.json();
+            resolve(this.results);
+          },
+          msg => { // Error
+            reject(msg);
+            }
+        );
+    });
+    // console.log('promise',promise);
+    return promise;
   }
+
  
 }
--- src/app/user-search/user-search.component.ts
+++ src/app/user-search/user-search.component.ts
@@ -1,3 +1,4 @@
+import { async } from '@angular/core/testing';
 import { Component, OnInit, ViewChild, OnDestroy} from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import {TweetObj,TweetServiceService} from '../tweet-service.service';
@@ -16,8 +17,8 @@ export class UserSearchComponent implements OnInit,OnDestroy {
   
   userForm: FormGroup;
   errormsg='';
-  allUserTwitts = []; // all the twitt form the service. Allow-Cross-Origin did not work
-  twittsWithUser = []; // the filtered Twitts by users
+  //allUserTwitts = []; // all the twitt form the service. Allow-Cross-Origin did not work
+  twittsWithUser; // the filtered Twitts by users
   currentPage = 1; // for pager and pagination
   totalItems = 9; // for pager and pagination
   maxSize = 5; // for pager
@@ -37,33 +38,25 @@ export class UserSearchComponent implements OnInit,OnDestroy {
     });
     
     // subscribing to the twitts
-    this.twittsObservableSubscription = this.userSearch.getAllUserTweets()
-    .subscribe(
-      (data) => {
-        this.allUserTwitts.push(JSON.parse(data));
-        this.totalItems = this.allUserTwitts.length;
-       // console.log('this.allUserTwitts.length',this.allUserTwitts.length);
-      },
-      (error: string) => { console.log(error); },
-      () => { console.log('completed in sub this.allUserTwitts.length',this.allUserTwitts.length); }
-    );
+    
+  //  this.allUserTwitts =  this.hashSearch.getAllTweets(hashtagStr);
+    // .subscribe(
+    //   (data) => {
+    //     this.allUserTwitts.push(JSON.parse(data));
+    //     this.totalItems = this.allUserTwitts.length;
+    //    // console.log('this.allUserTwitts.length',this.allUserTwitts.length);
+    //   },
+    //   (error: string) => { console.log(error); },
+    //   () => { console.log('completed in sub this.allUserTwitts.length',this.allUserTwitts.length); }
+    // );
 
-    this.twittsWithUser = [...this.allUserTwitts]; 
+   // this.twittsWithUser = [...this.allUserTwitts]; 
   }
   
-  onSubmit(){
-  
-   // this.twittsWithUser = [...this.allUserTwitts]; // starting with all twitts
-    // filtering the twitts array by users recursivly
-
-    this.twittsWithUser = this.twittsWithUser.filter( twitt => {
-      twitt.account.fullname ===this.userForm.value.user
-    });
-
+  async onSubmit(){
+    this.twittsWithUser = await this.userSearch.getTweetsByUser(this.userForm.value.user);
     this.totalItems = this.twittsWithUser.length;
-    console.log('this.twittsWithUser.length',this.twittsWithUser.length);
-
-    // this.userForm.reset();
+    console.log('this.twittsWithUser',this.twittsWithUser);
   }
 
 
